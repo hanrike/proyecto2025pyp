ab248186051092b1402da14bd9a87fb2bbfd3203
Jarol <jaancastano@utp.edu.co>
2025-10-28 08:15:25 -0500

    Instalacion tailwind con version 2021 profe daniel a pesr de eso y agregar clases no funcionan estilos

--- src/layouts/PrivateLayout.jsx
+++ src/layouts/PrivateLayout.jsx
@@ -1,6 +1,6 @@
-import { Outlet } from 'react-router-dom'
-import Sidebar from 'components/Sidebar'
-import React from 'react'
+import { Outlet } from 'react-router-dom';
+import Sidebar from 'components/Sidebar';
+import React from 'react';
 
 
 const PrivateLayout = () => {
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,5 @@
 
-import React from 'react'
+import React from 'react';
 
 const Login = () => {
   return (
@@ -9,4 +9,4 @@ const Login = () => {
   )
 }
 
-export default Login
+export default Login;
